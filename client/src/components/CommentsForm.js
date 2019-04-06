0118fd4b9c7b5a9df3f933cb23af65d306f53744
Jason Chu <jasonchu721@gmail.com>
2019-04-06 11:22:52 -0600

    added comments and started comments form

---
+++ client/src/components/CommentsForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios';
+import { Form, Header, Dropdown, Container } from "semantic-ui-react";
+
+class CommentsForm extends React.Component {
+  render () {
+    return {
+      
+    }
+  }
+}
+
+export default CommentsForm
