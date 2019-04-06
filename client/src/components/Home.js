d5d73d349757cf96e981fb91e77ab59587b3cb78
Joshua Larsen <larsen.bass@gmail.com>
2019-04-06 11:14:48 -0600

    took off home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Header, } from 'semantic-ui-react';
-import VideoShow from './VideoShow'
+
 
 const Home = () => (
   <>
   
   <Header as="h3" textAlign="center">Devise Auth App</Header>
   <iframe width="560" height="315" src="https://www.youtube.com/embed/x3y4WdN4PI8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  <VideoShow />
   </>
 )
 
